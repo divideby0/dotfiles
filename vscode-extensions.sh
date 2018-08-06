53399b3a571c03a224c4f2c9024e7911ade4f6bf
Cedric Hurst <cedric@spantree.net>
2018-08-06 16:50:21 -0500

    adding my vscode extensions

---
+++ vscode-extensions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# via: code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension josa.markdown-table-formatter
+code --install-extension mauve.terraform
+code --install-extension mquandalle.graphql
+code --install-extension ms-mssql.mssql
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.Go
+code --install-extension redhat.java
+code --install-extension redhat.vscode-yaml
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension vstirbu.vscode-mermaid-preview
